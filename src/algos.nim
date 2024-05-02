8b9cab283837e1db7ddff30805dbab83d4891744
hamidb80 <hr.bolouri@gmail.com>
2024-05-02 22:41:37 +0330

    add BFS

--- src/algos.nim
+++ src/algos.nim
@@ -1,4 +1,4 @@
-import std/[options, sequtils, sets, hashes]
+import std/[options, sequtils, sets, hashes, deques, tables, algorithm]
 
 # structures ------
 
@@ -16,6 +16,7 @@ type
         row, col: int
 
     Path = seq[Location]
+    Journey = Slice[Location]
 
     ResultPack = object
         visits*: seq[Location]
@@ -23,7 +24,7 @@ type
 
     PathFindingFnWithStep* = proc(
         map: Map[Cell], 
-        start, goal: Location,
+        journey: Journey,
     ): ResultPack
 
 # utils ------
@@ -32,14 +33,16 @@ func popd(s: var seq) =
     ## delete the last index
     del s, s.high
 
+func empty[T](s: T): bool = 
+    0 == len s
 
 func `+`(loc: Location, vec: Vector2): Location = 
     (loc.row + vec.y, loc.col + vec.x)
 
 
 func initMap*[T](rows, cols: Positive, init: T): Map[T] =
-  let row = newSeqWith(cols, init)
-  newSeqWith(rows, row)
+  newSeqWith rows:
+    newSeqWith cols, init
 
 func width(map: Map): Natural = 
     len map[0]
@@ -60,7 +63,16 @@ const moves = [
     ( 0, -1).Vector2,
     (+1,  0),
     ( 0, +1),
-    (-1,  0)]
+    (-1,  0),
+]
+
+func canGo(
+    loc:  Location, 
+    map:  Map[Cell], 
+): bool =
+    loc      in    map  and 
+    map[loc] !=    wall    
+
 
 proc dfsImpl(
     map: Map[Cell], 
@@ -76,66 +88,75 @@ proc dfsImpl(
     else:
         for m in moves:
             let loc = current + m
-            if  loc      in    map and 
-                map[loc] !=    wall and 
-                loc      notin seen:
-                
-                add result.visits, loc
-                add path, loc
-                dfsImpl map, loc, goal, seen, path, result
-                popd path
-    
-proc bfsImpl(
-    map: Map[Cell], 
-    current, goal: Location, 
-    seen:   var HashSet[Location],
-    path:   var Path,
-    result: var ResultPack
-) = 
-    incl seen, current
-
-    if current == goal:
-        result.finalPath = some path 
-    else:
-        for m in moves:
-            let loc = current + m
-            if  loc in map and 
-                map[loc] != wall and 
+            if  loc.canGo(map) and 
                 loc notin seen:
-                
-                add result.visits, loc
-                add path, loc
+                add     result.visits, loc
+                add     path, loc
                 dfsImpl map, loc, goal, seen, path, result
-                popd path
-
-
-proc dfs*(map: Map[Cell], start, goal: Location): ResultPack = 
+                popd    path
+    
+proc dfs*(map: Map[Cell], journey: Journey): ResultPack = 
     var
         seen: HashSet[Location]
-        path: Path
-    dfsImpl map, start, goal, seen, path, result
+        path: Path = @[journey.a]
+    dfsImpl map, journey.a, journey.b, seen, path, result
 
-proc bfs*(map: Map[Cell], start, goal: Location): ResultPack = 
+
+proc follow(tail, head: Location, track: Table[Location, Location]): Path = 
+    var c   = tail
+    add result, c
+    while c != head:
+        c = track[c]
+        add result, c
+
+    reverse result
+
+proc bfs*(map: Map[Cell], journey: Journey): ResultPack = 
     var
-        seen: HashSet[Location]
-        path: Path
-    bfsImpl map, start, goal, seen, path, result
+        track: Table[Location, Location]
+        queue = initDeque[Location]()
 
-proc aStar*(map: Map[Cell], start, goal: Location): ResultPack = 
+    addfirst queue, journey.a
+    while not empty queue: 
+        let c = popLast queue
+        add result.visits, c
+        if  c == journey.b:
+            result.finalPath = some follow(journey.b, journey.a, track)
+            return
+        else:
+            for m in moves:
+                let n = c + m
+                if  n.canGo(map) and 
+                    n notin track:
+                    addlast queue, n
+                    track[n] = c
+
+
+proc aStar*(map: Map[Cell], journey: Journey): ResultPack = 
     # https://github.com/Nycto/AStarNim
     discard
 
 
 when isMainModule:
+    import print
+
     const 
         W = wall
         F = free
     let map = @[
         @[W,W,W,W,W,W,W],
-        @[W,F,F,F,F,F,W],
+        @[W,F,F,W,F,F,W],
         @[W,F,F,F,F,F,W],
         @[W,W,W,W,W,W,W],
     ]
 
-    let r = dfs(map, (2, 1), (1, 5))
-    echo r
+    let  
+        journey = (2, 1) .. (1, 5)
+        d = dfs(  map, journey)
+        b = bfs(  map, journey)
+        a = aStar(map, journey)
+
+    print journey
+    print d
+    print b
+    print a
